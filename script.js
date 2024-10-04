7b565eaf3282cb6c425ef6c35f02d336222cf3fb
CC3636-IV <johansen.morten.m@gmail.com>
2024-10-04 13:53:06 +0200

    JS Done For Basic TodoList

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
 let userInput = document.querySelector("#user-input");
-let todolistelement = document.querySelector("#todo-list");
+let todoListElement = document.querySelector("#todo-list");
+
+let todos = [];
 
 function handleSubmit(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    console.log("Creating Todo Object...");
-    createTodoObject(userInput);
+  console.log("Creating Todo Object...");
+  let newTodo = createTodoObject(userInput);
+
+  console.log("Append new todo to todo list...");
+  todos.push(newTodo);
+
+  console.log("Clearing out the old todos from the document...");
+  todoListElement.innerHTML = "";
+
+  console.log("Appending all todos to the Document...");
+  todos.forEach((todo, index) => {
+    let newTodoCard = createTodoCard(todo);
+    todoListElement.append(newTodoCard);
+  });
 }
 
 userInput.addEventListener("submit", handleSubmit);
 
 function createTodoObject(form) {
-    let todo = form.querySelector("#todo");
-    let todoValue = todo.value;
-    console.dir(todoValue);
+  let todo = form.querySelector("#todo");
+  let todoValue = todo.value;
+
+  let todoObject = {
+    title: todoValue,
+  };
+
+  return todoObject;
+}
+
+function createTodoCard(todoObject) {
+  let todoCard = document.createElement("li");
+  let titleElement = document.createElement("h2");
+
+  todoCard.append(titleElement);
+
+  titleElement.textContent = todoObject.title;
+
+  return todoCard;
 }
